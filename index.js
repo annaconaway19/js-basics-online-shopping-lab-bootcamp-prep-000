62fbf2ce07ee72befb76194f4b7605711b311fca
Anna Conaway <annaconaway19@gmail.com>
2018-07-28 17:21:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function total() {
 }
 
 function removeFromCart(item) {
-   for (var i = 0; i < cart.length; i++) {
+  for (var i = 0; i < cart.length; i++) {
     if (cart[i]["itemName"] === item) {
        cart.splice(0, cart[i]["itemName"]);
    return cart;
