4d94212f417a202219e9b5079cc2cc48087d8ce8
Anna Conaway <annaconaway19@gmail.com>
2018-07-28 16:40:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   if (cardNumber) {
     var emptyCart = [];
-    emptyCart;
+    emptyCart.push(cart.splice(0));
     return `Your total cost is $${total(cart)}, which will be charged to the card ${cardNumber}.`;
   } else {
   return "Sorry, we don't have a credit card on file for you.";
