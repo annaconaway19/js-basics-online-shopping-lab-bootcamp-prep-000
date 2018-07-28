b9c422407479b00d2e20a2acb14b2f73808be1ad
Anna Conaway <annaconaway19@gmail.com>
2018-07-28 15:41:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,13 +19,16 @@ function addToCart(item) {
 function viewCart() {
   if (cart.length === 0) {
     return 'Your shopping cart is empty.'
+  } else if (cart.length = 1) {
+    return `In your cart, you have ${cart[0]["itemName"]} at $${cart[0]["itemPrice"]}.`
+    
   } else if (cart.length > 0) {
     var itemAndPrices = [];
     for (var i = 0; i < cart.length; i++) {
       itemAndPrices.push(`${cart[i]["itemName"]} at $${cart[i]["itemPrice"]}`);
-    return `In your cart, you have ${itemAndPrices}.`
           }    
       } else if (cart.length === 2) {
+        
         return `In your cart, you have ${itemAndPrices.join(' , and ')}.`
       } else if (cart.length >= 3) {
         return `In your cart, you have ${itemAndPrices.join('  ,   ,  and ')}`
